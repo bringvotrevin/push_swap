03e342bb519e783b124a7d800cb61e0b76d798a8
Dasom Im <dim@c8r8s10.42seoul.kr>
2021-05-30 22:22:43 +0900

    0530

--- parse.c
+++ parse.c
@@ -6,7 +6,7 @@
 /*   By: dim <dim@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/28 17:26:06 by dim               #+#    #+#             */
-/*   Updated: 2021/05/28 18:25:28 by dim              ###   ########.fr       */
+/*   Updated: 2021/05/30 21:55:24 by dim              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,21 +14,27 @@
 
 void	push_swap(int argc, char *argv[])
 {
-	t_st	stack_a;
-	t_st	stack_b;
+	t_st	*stack_a;
+	t_st	*stack_b;
 
+	stack_a = NULL;
+	stack_b = NULL;
 	if (argc < 2)
 		return 0;
-
+	check_error();
 	parse(argc, argv, &stack_a);
-
 
 	return (0);
 }
 
-void	parse(int argc, char **argv, t_st *head)
+void	parse(int argc, char **argv, t_st **head)
 {
-	
+	int		i;
+
+	while (i < argc)
+	{
+		
+	}
 }
 
  void	sa()
