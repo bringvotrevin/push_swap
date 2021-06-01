4d686b637ba5a318a6f79616c9c65147fa783210
dim <dim@student.42seoul.kr>
2021-06-01 11:49:25 +0900

    0601 remove pointer

--- parse.c
+++ parse.c
@@ -6,7 +6,7 @@
 /*   By: dim <dim@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/28 17:26:06 by dim               #+#    #+#             */
-/*   Updated: 2021/05/31 21:54:29 by dim              ###   ########.fr       */
+/*   Updated: 2021/06/01 11:48:28 by dim              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,20 +14,20 @@
 
 void	push_swap(int argc, char *argv[])
 {
-	t_lst	*tail_a;
-	t_lst	*tail_b;
+	t_lst	tail_a;
+	t_lst	tail_b;
 
-	tail_a = lst_new();
+	tail_a = NULL;
 	if (argc < 2)
 		return (0);
 	check_error();
-	parse(argc, argv, tail_a);
+	parse(argc, argv, &tail_a);
 	return (0);
 }
 
 void	parse(int argc, char **argv, t_st *tail_a)
 {
-	t_st	*stack_a;
+	t_st	*newn;
 	int		i;
 	char	*num;
 
@@ -36,16 +36,18 @@ void	parse(int argc, char **argv, t_st *tail_a)
 	{
 		num = ft_atol(argv[i]);
 		stack_a = ft_lstnew(num);
-		ft_lstadd_tail(&(tail_a->tail), stack_a); 
+		ft_lstadd_tail(tail_a, new); 
 	}
 	tail_a->tail = stack_a;
 }
 
-void	ft_lstadd_tail(t_st **tail, t_st *stack_a)
+void	ft_lstadd_tail(t_st *tail, t_st *stack_a)
 {
 	if (tail == NULL || stack_a = NULL)
 		return ;
-	**tail->next = stack_a;
+	tail->next = stack_a;
 	*tail = stack_a;
 
  void	sa(tail_a)
+ {
+
