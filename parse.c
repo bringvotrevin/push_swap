fe8906ba20e47a142c9ed6fc0b0a514d9053a8db
Dasom Im <dim@c8r8s10.42seoul.kr>
2021-06-02 19:18:52 +0900

    0602 rr, rrr

--- parse.c
+++ parse.c
@@ -6,7 +6,7 @@
 /*   By: dim <dim@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/28 17:26:06 by dim               #+#    #+#             */
-/*   Updated: 2021/06/02 14:51:22 by dim              ###   ########.fr       */
+/*   Updated: 2021/06/02 17:51:45 by dim              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -73,7 +73,7 @@ void	ft_lstadd_tail(t_lst *tail_lst, t_st *new)
 	}
 }
 
-void	ft_swap(t_lst stack)
+void	ft_swap(t_st *stack)
 {
 	int		temp;
 
@@ -82,19 +82,74 @@ void	ft_swap(t_lst stack)
 	stack->next->next->num = temp;
 }
 
-void	sa(t_st tail_a)
+void	sa(t_lst *tail_a)
 {
 	ft_swap(tail_a->tail);
+	write(1, "sa\n", 3);
 }
 
-void	sb(t_st tail_b)
+void	sb(t_lst *tail_b)
 {
 	ft_swap(tail_b->tail);
+	write(1, "sb\n", 3);
 }
 
-void	ft_rotate(t_lst stack)
+void	ss(t_lst *tail_a, t_lst *tail_b)
 {
-	
+	ft_swap(tail_a->tail);
+	ft_swap(tail_b->tail);
+	write(1, "ss\n", 3);
+}
 
+void	ft_rotate(t_lst *tail_lst)
+{
+	tail_lst->tail = tail_lst->tail->next;
+}
+
+void	ra(t_lst *tail_a)
+{
+	ft_rotate(tail_a);
+	write(1, "ra\n", 3);
+}
+
+void	rb(t_lst *tail_b)
+{
+	ft_rotate(tail_b);
+	write(1. "rb\n" 3);
+}
+
+void	rr(t_lst *tail_a, t_lst *tail_b)
+{
+	ft_rotate(tail_a);
+	ft_rotate(tail_b);
+	write(1, "rr\n", 3);
+}
+
+void	ft_rev_rotate(t_lst *tail_lst)
+{
+	tail_lst->tail = tail_lst->tail->prev;
+}
+
+void	rra(t_lst *tail_a)
+{
+	ft_rev_rotate(tail_a);
+	write(1, "rra\n", 4);
+}
+
+void	rrb(t_lst *tail_b)
+{
+	ft_rev_rotate(tail_b);
+	write(1, "rrb\n", 4);
+}
+
+void	rrr(t_lst *tail_a, t_lst *tail_b)
+{
+	ft_rev_rotate(tail_a);
+	ft_rev_rotate(tail_b);
+	write(1, "rrr\n", 4);
+}
+
+void	ft_push(t_st *stack_loss, t_st *stack_gain)
+{
 
 
